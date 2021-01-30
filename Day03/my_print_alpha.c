285eb08b38df2c103de595cdd4096019723d1d2b
Boumerxp <bassel-battefieldlien@hotmail.fr>
2021-01-30 20:29:36 +0100

    Task01
    alphabet

---
+++ Day03/my_print_alpha.c
@@ -0,0 +1,13 @@
+/*
+** Tryhard PROJECT, 2021
+** LearnC
+** File description:
+** my_print_alpha
+*/
+
+#include <unistd.h>
+
+int my_print_alpha (void)
+{
+    write(1,"abcdefghijqlmnopqrstuvwxyz", 27);
+}
