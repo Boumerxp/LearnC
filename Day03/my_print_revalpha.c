907f6d7affdb9e3fe65837cb51c408d0ca7571d8
Boumerxp <bassel-battefieldlien@hotmail.fr>
2021-01-30 20:33:07 +0100

    Task02
    reverse alphabet

---
+++ Day03/my_print_revalpha.c
@@ -0,0 +1,13 @@
+/*
+** Tryhard PROJECT, 2021
+** LearnC
+** File description:
+** my_print_revalpha
+*/
+
+#include <unistd.h>
+
+int my_print_revalpha (void)
+{
+    write(1,"zyxwvutsrqponmlqjihgfedcba", 27);
+}
