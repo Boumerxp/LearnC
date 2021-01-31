e4e63001d8c7a5baad98ddc00f643a0637df4284
Boumerxp <bassel-battefieldlien@hotmail.fr>
2021-01-31 22:08:40 +0100

    Ascii

--- Day03/my_put_nbr.c
+++ Day03/my_put_nbr.c
@@ -5,7 +5,35 @@
 ** my_put_nbr
 */
 
-int my_put_nbr (int nb )
+#include <unistd.h>
+
+int my_put_nbr(int nb)
 {
-    
+    if (nb <= 9)
+    {
+        char value = nb + '0';
+        write(1, &value, 1);
+    }
+    else if (nb > 9 && nb < 100){
+        char value = nb / 10 + '0'; // 15 / 10 = 1.5
+        char value2 = nb % 10 + '0'; // modulo de nb
+        write(1, &value, 1);
+        write(1, &value2, 1);
+    }
+    else
+    {
+        if (nb > 0)
+        {
+            char value = nb % 10 + '0';
+            char value2 = value;
+            value = value / 10 + '0';
+            my_put_nbr(value2);
+        }
+    }
+}
+
+int main(int a)
+{
+    my_put_nbr(155);
+    return 0;
 }
